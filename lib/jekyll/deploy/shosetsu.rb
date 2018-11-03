56d079cc3b3d23ae8da2fddc56af3e7eea91aa8e
Fuji Nakahara <fujinakahara2032@gmail.com>
2018-11-03 20:43:19 +0900

    Initial commit

---
+++ lib/jekyll/deploy/shosetsu.rb
@@ -0,0 +1,10 @@
+require "jekyll/deploy/shosetsu/version"
+
+module Jekyll
+  module Deploy
+    module Shosetsu
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jekyll/deploy/shosetsu/version.rb
@@ -0,0 +1,7 @@
+module Jekyll
+  module Deploy
+    module Shosetsu
+      VERSION = "0.1.0"
+    end
+  end
+end
